21c6f8303fc4f1b575bccf5dab4fa9e043308a28
SACHIN KN <38110693+sachinkn777@users.noreply.github.com>
2021-07-08 20:25:37 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "project" { 
+    default = "terraform-318907"
+}
+
+variable "region" {
+  default = "asia-south1"
+}
+
+variable "zone" {
+  default = "asia-south1-a"
+}
+variable "network1" {
+  default = "terraform-network1"
+}
+
+variable "network2" {
+  default = "terraform-network2"
+}
+variable "serviceaccount" {
+  default ="terraform-serviceaccount@terraform-318907.iam.gserviceaccount.com"
+}
